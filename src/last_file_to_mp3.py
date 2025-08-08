e5100773e9d1f42e97682a661aaec08eeeb34dc2
keyboardmedic <keyboardmedic@proton.me>
2025-08-08 12:38:02 +0200

    refactor: implemented housey_logging

--- src/last_file_to_mp3.py
+++ src/last_file_to_mp3.py
@@ -1,3 +1,6 @@
+import housey_logging
+housey_logging.configure()
+
 import requests
 from os.path import exists
 import os
@@ -6,6 +9,7 @@ import subprocess
 import datetime
 import time
 import re
+import logging
 
 # vars
 # do not touch anything above this!!!
@@ -32,16 +36,16 @@ current_date = datetime.date.today().strftime("%B %d, %Y")
 def get_twitch_title(token: str) -> tuple[str,dict]:
 
     response=requests.get(f"https://api.twitch.tv/helix/channels?broadcaster_id={twitch_user_id}",headers={'Authorization':f"Bearer {token}",'Client-Id':twitch_api_username})
-    print(f"tried to get twitch title with function get_twitch_title for {twitch_user_id} with response: {response}")
+    logging.info(f"tried to get twitch title with function get_twitch_title for {twitch_user_id} with response: {response}")
     responsejson = response.json()
     twitch_title = responsejson["data"][0]["title"]
-    print(f"twitch title gotten from twitch api is = {twitch_title}")
+    logging.info(f"twitch title gotten from twitch api is = {twitch_title}")
 
     return(twitch_title,response)
 
 def get_twitch_api_token() -> str: 
         
-        print("Requesting new twitch api auth token from twitch")
+        logging.info("Requesting new twitch api auth token from twitch")
         response=requests.post(
                             "https://id.twitch.tv/oauth2/token",
                             json={"client_id" : str(twitch_api_username),
@@ -52,13 +56,13 @@ def get_twitch_api_token() -> str:
         if response.ok:
             token_json = response.json()
             token = token_json["access_token"]
-            print(f"new twitch api auth token is: {token}")
+            logging.info(f"new twitch api auth token recieved from twitch")
 
             with open(r'token.txt', 'w') as tokenFile:
                 tokenFile.write("%s\n" % token)
 
         else:
-            print(f"unable to request new twitch api auth token with response: {response}")
+            logging.error(f"unable to request new twitch api auth token with response: {response}")
             token = "empty"
 
         return(token)
@@ -69,7 +73,7 @@ def get_twitch_api_token_from_file() -> str:
             with open("token.txt", 'r') as file2:
                 tokenRaw = str(file2.readline())
                 token = tokenRaw.strip()
-            print ("twitch api auth token to use for auth: " + token)
+            logging.info ("twitch api auth token to use loaded from file")
 
     else:
         token = get_twitch_api_token()
@@ -90,7 +94,7 @@ def convert_video_to_audio_with_ffmpeg(video_file_path: str, audio_file_path: st
     subprocess.call(command, shell=True)
 
 def send_message_to_discord():
-    print(f"waiting {str(discord_message_wait_time_minutes)} minutes before posting a notification to discord")
+    logging.info(f"waiting {str(discord_message_wait_time_minutes)} minutes before posting a notification to discord")
 
     if discord_message_wait_time_minutes > 0:
         time.sleep(60*discord_message_wait_time_minutes)
@@ -98,10 +102,7 @@ def send_message_to_discord():
     data_for_hook = {"content": f"{discord_message}"}
     discord_webhook_response = requests.post(discord_webhook, json=data_for_hook, params={'wait': 'true'})
 
-    if discord_webhook_response.ok:
-        print(f"posting message to discord, response is {discord_webhook_response}")
-    else:
-        print(f"attempted to post message to discord, response is {discord_webhook_response}")
+    logging.info(f"posting message to discord, response is {discord_webhook_response}")
 
 def sanitize_filename(name: str, replacement: str="_") -> str:
     sanitized = re.sub(r'[<>:"/\\|?*\x00-\x1F]', replacement, name)
@@ -123,19 +124,19 @@ def main():
     # sorts files in input folder by date (last one first) and gets last file
     sorted_files = sorted(glob.glob(f"{input_folder_path}*"), key=os.path.getctime, reverse=True)
     video_file = f'"{str(sorted_files[0])}"'
-    print(f"latest video file = {video_file}")
+    logging.info(f"latest video file = {video_file}")
 
     # adds current date to file title (twitch title) and starts conversion
     file_title_stripped_of_invalid_characters = sanitize_filename(file_title)
     audio_file = f'"{output_file_path}{file_title_stripped_of_invalid_characters} - {current_date}.mp3"'
-    print(f"getting ready to export to {audio_file}")
+    logging.info(f"getting ready to export to {audio_file}")
     convert_video_to_audio_with_ffmpeg(video_file, audio_file, file_title_stripped_of_invalid_characters)
 
     #sends notification to discord about your new mix
     if use_discord_notification:
         send_message_to_discord()
 
-    print(f"script finished...")
+    logging.info(f"script finished...")
 
 if __name__ == "__main__":
     main()
