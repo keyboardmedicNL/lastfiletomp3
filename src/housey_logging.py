1beb5ca10da09d445fb5ddeb820beb8e5d6f03ca
keyboardmedic <keyboardmedic@proton.me>
2025-08-08 12:33:13 +0200

    add: added housey_logging

---
+++ src/housey_logging.py
@@ -0,0 +1,17 @@
+import logging
+
+
+def configure():
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s: [%(module)s.%(funcName)s:%(lineno)d]: %(message)s',
+        filename='last_file_to_mp3.log',
+        level=logging.INFO
+    )
+
+    console = logging.StreamHandler()
+    console.setLevel(level=logging.DEBUG)
+    console.setFormatter(
+        logging.Formatter('%(asctime)s %(levelname)s: [%(module)s.%(funcName)s:%(lineno)d]: %(message)s')
+    )
+    logging.getLogger().addHandler(console)
+
