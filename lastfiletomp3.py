5d79e189580a986529344d7e58e9b31056cd4d64
keyboardmedic <keyboardmedic@proton.me>
2025-08-08 12:27:42 +0200

    refactor: added typehints and made main a function

--- lastfiletomp3.py
+++ lastfiletomp3.py
@@ -28,9 +28,8 @@ use_discord_notification = True
 
 current_date = datetime.date.today().strftime("%B %d, %Y")
 
-
 # functions
-def get_twitch_title():
+def get_twitch_title(token: str) -> tuple[str,dict]:
     response=requests.get(
                         f"https://api.twitch.tv/helix/channels?broadcaster_id={twitch_user_id}",
                         headers={'Authorization':f"Bearer {token}",
@@ -38,16 +37,12 @@ def get_twitch_title():
                         )
     print(f"tried to get twitch title with function get_twitch_title for {twitch_user_id} with response: {response}")
     responsejson = response.json()
-
-    try:
-        twitch_title = responsejson["data"][0]["title"]
-    except:
-        twitch_title = "unknown title"
+    twitch_title = responsejson["data"][0]["title"]
     print(f"twitch title gotten from twitch api is = {twitch_title}")
 
     return(twitch_title,response)
 
-def get_twitch_api_token(): 
+def get_twitch_api_token() -> str: 
         print("Requesting new twitch api auth token from twitch")
         response=requests.post(
                             "https://id.twitch.tv/oauth2/token",
@@ -56,7 +51,7 @@ def get_twitch_api_token():
                             "grant_type":"client_credentials"}
                             )
         
-        if "200" in str(response):
+        if response.ok:
             token_json = response.json()
             token = token_json["access_token"]
             print(f"new twitch api auth token is: {token}")
@@ -69,7 +64,7 @@ def get_twitch_api_token():
 
         return(token)
 
-def get_twitch_api_token_from_file():
+def get_twitch_api_token_from_file() -> str:
     if exists(f"token.txt"):
             with open("token.txt", 'r') as file2:
                 tokenRaw = str(file2.readline())
@@ -80,7 +75,7 @@ def get_twitch_api_token_from_file():
         
     return(token)
 
-def convert_video_to_audio_with_ffmpeg(video_file_path, audio_file_path):
+def convert_video_to_audio_with_ffmpeg(video_file_path: str, audio_file_path: str, file_title: str):
     command = (
          f'ffmpeg -i {{}} '
          f'-vn '
@@ -99,42 +94,46 @@ def send_message_to_discord():
         time.sleep(60*discord_message_wait_time_minutes)
 
     data_for_hook = {"content": f"{discord_message}"}
-    rl = requests.post(discord_webhook, json=data_for_hook, params={'wait': 'true'})
+    discord_webhook_response = requests.post(discord_webhook, json=data_for_hook, params={'wait': 'true'})
 
-    if "200" in str(rl):
-        print(f"posting message to discord, response is {rl}")
+    if discord_webhook_response.ok:
+        print(f"posting message to discord, response is {discord_webhook_response}")
     else:
-        print(f"attempted to post message to discord, response is {rl}")
+        print(f"attempted to post message to discord, response is {discord_webhook_response}")
 
-def sanitize_filename(name, replacement="_"):
+def sanitize_filename(name: str, replacement: str="_") -> str:
     sanitized = re.sub(r'[<>:"/\\|?*\x00-\x1F]', replacement, name)
     sanitized = sanitized.strip(" .")
 
     return sanitized if sanitized else "unnamed"
 
 #main
-#gets token from file or api and loads it to token var
-token = get_twitch_api_token_from_file()
+def main():
+    #gets token from file or api and loads it to token var
+    token = get_twitch_api_token_from_file()
 
-#attempts to get twitch title from twitch api with if statement incase twitch api token is invalid
-file_title, get_twitch_title_response = get_twitch_title()
-if not "200" in str(get_twitch_title_response):
-    token = get_twitch_api_token()
-    file_title, get_twitch_title_response = get_twitch_title()
+    #attempts to get twitch title from twitch api with if statement incase twitch api token is invalid
+    file_title, get_twitch_title_response = get_twitch_title(token)
+    if not get_twitch_title_response.ok:
+        token = get_twitch_api_token()
+        file_title, get_twitch_title_response = get_twitch_title(token)
 
-# sorts files in input folder by date (last one first) and gets last file
-sorted_files = sorted(glob.glob(f"{input_folder_path}*"), key=os.path.getctime, reverse=True)
-video_file = f'"{str(sorted_files[0])}"'
-print(f"latest video file = {video_file}")
+    # sorts files in input folder by date (last one first) and gets last file
+    sorted_files = sorted(glob.glob(f"{input_folder_path}*"), key=os.path.getctime, reverse=True)
+    video_file = f'"{str(sorted_files[0])}"'
+    print(f"latest video file = {video_file}")
 
-# adds current date to file title (twitch title) and starts conversion
-file_title_stripped_of_invalid_characters = sanitize_filename(file_title)
-audio_file = f'"{output_file_path}{file_title_stripped_of_invalid_characters} - {current_date}.mp3"'
-print(f"getting ready to export to {audio_file}")
-convert_video_to_audio_with_ffmpeg(video_file, audio_file)
+    # adds current date to file title (twitch title) and starts conversion
+    file_title_stripped_of_invalid_characters = sanitize_filename(file_title)
+    audio_file = f'"{output_file_path}{file_title_stripped_of_invalid_characters} - {current_date}.mp3"'
+    print(f"getting ready to export to {audio_file}")
+    convert_video_to_audio_with_ffmpeg(video_file, audio_file, file_title_stripped_of_invalid_characters)
 
-#sends notification to discord about your new mix
-if use_discord_notification:
-    send_message_to_discord()
+    #sends notification to discord about your new mix
+    if use_discord_notification:
+        send_message_to_discord()
 
-print(f"script finished...")
+    print(f"script finished...")
+
+if __name__ == "__main__":
+    main()
